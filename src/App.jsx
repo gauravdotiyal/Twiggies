488671926b90a2285417fdaf47098b494f7aa9ad
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-06-06 19:39:39 +0530

    footer part completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import "./App.css"
-import Navbar from './components/Navbar/Navbar'
-import {Route,Routes} from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className='app'>
-        <Navbar/>
+    <>
+        <div className="app">
+          <Navbar /> 
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/PlaceOrder" element={<PlaceOrder />} />
+            </Routes>
+        </div>
+        <Footer />
+    </>
+  );
+};
 
-        <Routes>
-            <Route exact path='/' element={<Home/>} /> 
-            <Route path='/cart' element={<Cart/>} /> 
-            <Route path='/PlaceOrder' element={<PlaceOrder/>} /> 
-        </Routes>
-    </div>
-  )
-}
-
-export default App
- 
+export default App;
