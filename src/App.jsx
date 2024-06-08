a907413351ef7322217237bd3798941011257064
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-06-08 14:49:56 +0530

    login page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
@@ -6,12 +6,18 @@ import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
+import LoginPopup from "./components/LoginPopUp/LoginPopup";
 
 const App = () => {
+
+   const [showLogin,setShowLogin]=useState(false)
+
+
   return (
-    <>
+    <> 
+        {showLogin && <LoginPopup setShowLogin={setShowLogin}/>}
         <div className="app">
-          <Navbar /> 
+          <Navbar setShowLogin={setShowLogin}/> 
             <Routes>
               <Route exact path="/" element={<Home />} />
               <Route path="/cart" element={<Cart />} />
