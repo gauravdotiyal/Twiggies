8c08c9b7cbffa3a30f6b68a6482100dce7ed4152
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-06-09 01:19:18 +0530

    cart section completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,68 @@
-import React, { useState } from 'react'
-import "./Navbar.css"
-import { assets } from '../../assets/assets'
-import {Link} from "react-router-dom"
+import React, { useContext, useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
 
-const Navbar = ({setShowLogin}) => {
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("menu");
 
-  const  [menu,setMenu]=useState("menu");
-     
+  const { getTotalCartAmount } = useContext(StoreContext);
 
   return (
-    <div className='navbar'>
-         {/* <img src={assets.logo} alt="" className='logo' /> */}
-         <div className="logo">
-             <h1>Twiggies</h1>
-         </div>
+    <div className="navbar">
+      {/* <img src={assets.logo} alt="" className='logo' /> */}
 
-          <ul className='navbar-menu'>
-             <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
-             <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
-             <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
-             <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
-          </ul>
+      <div className="logo">
+        <Link exact to="/">
+          {" "}
+          <h1>Twiggies</h1>
+        </Link>
+      </div>
 
-          <div className="navbar-right">
-                <img src={assets.search_icon} alt="" />
-                <div className='navbar-search-icon'>
-                    <img src={assets.basket_icon} alt="" />
-                    <div className='dot'></div>
-                </div>
-                <button onClick={()=>setShowLogin(true)}>sign in</button>
-          </div>
-         
+      <ul className="navbar-menu">
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </a>
+      </ul>
+
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
+        </div>
+        <button onClick={() => setShowLogin(true)}>sign in</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,12 +1,82 @@
-import React from 'react'
-import './Cart.css'
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
+import { assets } from "../../assets/assets";
 
 const Cart = () => {
-  return (
-    <div>
-         Cart
-    </div>
-  )
-}
+  const { cartItems, removeFromCart, food_list, getTotalCartAmount } =
+    useContext(StoreContext);
 
-export default Cart
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x
+                  </p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+        })}
+      </div>
+
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="card-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="card-total-details">
+              <p>Delivery Fee</p>
+              <p>${2}</p>
+            </div>
+            <hr />
+            <div className="card-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+
+        <div className="card-promo-code">
+          <div>
+            <p>If you have Promocode Enter Here </p>
+            <div className="cart-promo-code-inputs">
+              <input type="text" placeholder="promo code" />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
