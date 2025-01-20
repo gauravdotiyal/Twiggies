f2293fe8e94a863aa701eaf516bacd01e37ce577
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2025-01-20 15:15:06 +0530

    Dollar updated to Rupees in Frontend Pages

--- Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -99,17 +99,17 @@ const PlaceOrder = () => {
           <div>
             <div className="card-total-details">
               <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
+              <p>₹{getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="card-total-details">
               <p>Delivery Fee</p>
-              <p>${getTotalCartAmount()===0?0:2}</p>
+              <p>₹{getTotalCartAmount()===0?0:2}</p>
             </div>
             <hr />
             <div className="card-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount()===0?0:getTotalCartAmount() + 2}</b>
+              <b>₹{getTotalCartAmount()===0?0:getTotalCartAmount() + 2}</b>
             </div>
           </div> 
           <button type="submit">
